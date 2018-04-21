609de99488b6b27ef92fbec7495a658b7998596a
Tiago Carneiro <web-tiago@hotmail.com>
2018-04-20 21:47:26 -0300

    Add project files.

---
+++ Android/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace Xamarin.Summit.Android
+{
+    [Activity(Label = "@string/app_name", Icon = "@mipmap/icon",
+        Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ Android/SplashScreen.cs
@@ -0,0 +1,26 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.Support.V7.App;
+using System;
+using System.Threading.Tasks;
+
+namespace Xamarin.Summit.Android
+{
+    [Activity(
+           Label = "@string/app_name"
+           , MainLauncher = true
+           , Icon = "@mipmap/icon"
+           , Theme = "@style/Theme.Splash"
+           , NoHistory = true
+           , ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashScreen : AppCompatActivity
+    {
+        protected async override void OnResume()
+        {
+            base.OnResume();
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
