ae21c86645f5b60da4b7334d786a6edf1bfc5d59
Tiago Carneiro <tiago.carneiro@inlog.com.br>
2018-04-24 18:24:40 -0300

    refactor(namespace): unused namespace removed

--- Android/SplashScreen.cs
+++ Android/SplashScreen.cs
@@ -2,8 +2,6 @@
 using Android.App;
 using Android.Content.PM;
 using Android.Support.V7.App;
-using System;
-using System.Threading.Tasks;
 
 namespace Xamarin.Summit.Android
 {
@@ -19,7 +17,6 @@ namespace Xamarin.Summit.Android
         protected async override void OnResume()
         {
             base.OnResume();
-            await Task.Delay(TimeSpan.FromSeconds(3));
             StartActivity(typeof(MainActivity));
         }
     }
