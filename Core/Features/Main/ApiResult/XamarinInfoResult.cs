3dd8b4ef37989cb8cdbf4e4a47f46cdfcf36eb47
Tiago Carneiro <tiago.carneiro@inlog.com.br>
2018-04-27 18:38:12 -0300

    feat(data): update date to control data downloaded

--- Core/Features/Main/ApiResult/XamarinInfoResult.cs
+++ Core/Features/Main/ApiResult/XamarinInfoResult.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Xamarin.Summit
 {
     public class XamarinInfoResult
     {
+        public string DataAtualizacao { get; set; }
         public InformacoesResult Informacoes { get; set; }
         public IEnumerable<ApoioResult> Apoio { get; set; }
         public IEnumerable<PessoaResult> Pessoas { get; set; }
-        public IEnumerable<AgendaResult> Agenda { get; set; }        
+        public IEnumerable<AgendaResult> Agenda { get; set; }
     }
 }
---
+++ Core/Features/Main/Models/KeyValueStorage.cs
@@ -0,0 +1,13 @@
+﻿using Realms;
+
+namespace Xamarin.Summit
+{
+    public class KeyValueStorage : RealmObject
+    {
+        public const string DataAtualizacao = "data_atualizacao";
+
+        [PrimaryKey]
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
