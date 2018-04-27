3a86a9ce6ffa7c3bfb2b559a56dc64642ba13ede
Tiago Carneiro <web-tiago@hotmail.com>
2018-04-26 22:12:59 -0300

    feat(api): Api rest data control class

---
+++ Core/Infrastructure/Api/ApiResult.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+
+namespace Xamarin.Summit
+{
+    public static class ApiResult
+    {
+        internal static ApiResult<T> Create<T>(T data, bool success, HttpStatusCode statusCode, string message = "")
+            => new ApiResult<T>(data, success, statusCode, message);
+    }
+
+    public class ApiResult<T>
+    {
+        public ApiResult(T data, bool success, HttpStatusCode statusCode, string message = "")
+        {
+            Data = data;
+            Message = message;
+            Success = success;
+            StatusCode = statusCode;
+        }
+
+        public T Data { get; private set; }
+
+        public bool Success { get; private set; }
+
+        public string Message { get; private set; }
+
+        public HttpStatusCode StatusCode { get; private set; }
+    }
+}
---
+++ Core/Infrastructure/Api/IApiRestfull.cs
@@ -0,0 +1,12 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace Xamarin.Summit
+{
+    [Headers("Content-Type: application/json")]
+    public interface IApiRestfull
+    {
+        [Get("/api/info")]
+        Task<XamarinInfoResult> GetInfo();
+    }
+}
