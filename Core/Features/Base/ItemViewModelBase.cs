c108d245b39e7fb629275a19e55e572891b05389
Tiago Carneiro <web-tiago@hotmail.com>
2018-04-28 15:29:54 -0300

    feat(viewModel): new viewModels to get list and item.

---
+++ Core/Features/Base/ItemViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace Xamarin.Summit
+{
+    public abstract class ItemViewModelBase<TModel> : ViewModelBase
+    {
+        private TModel _item;
+        public TModel Item
+        {
+            get => _item;
+            set => SetProperty(ref _item, value);
+        }
+
+        protected ItemViewModelBase(string title) : base(title)
+        {
+        }
+
+        protected abstract Task<TModel> GetItemAsync();
+
+        public override async Task InitializeAsync()
+            => Item = await GetItemAsync();
+    }
+}
---
+++ Core/Features/Base/ListViewModelBase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Xamarin.Summit
+{
+    public abstract class ListViewModelBase<TModel> : ViewModelBase where TModel : class
+    {
+        public ObservableCollection<TModel> Items { get; }
+
+        public ICommand ItemClickCommand { get; }
+
+        protected ListViewModelBase(string title) : base(title)
+        {
+            Items = new ObservableCollection<TModel>();
+            ItemClickCommand = new Command<TModel>(async (item) => await ItemClickCommandExecuteAsync(item));
+        }
+
+        protected abstract Task<IEnumerable<TModel>> GetItemsAsync();
+
+        protected virtual async Task ItemClickCommandExecuteAsync(TModel model) { }
+
+        public override async Task InitializeAsync()
+        {
+            var result = await GetItemsAsync();
+            Items.Clear();
+            result.ToList().ForEach(item => Items.Add(item));
+        }
+    }
+}
