281e59f371db1bb2d628309a758a9f945b2d4555
Tiago Carneiro <web-tiago@hotmail.com>
2018-04-27 21:45:42 -0300

    feat(service): BaseService creation

---
+++ Core/Features/Base/BaseService.cs
@@ -0,0 +1,49 @@
+﻿using Realms;
+
+namespace Xamarin.Summit
+{
+    public class BaseService
+    {
+        ulong SchemaVersion => 0;
+        protected Realm GetRealmInstance()
+        {
+            var config = new RealmConfiguration { SchemaVersion = SchemaVersion };
+
+            #region Migration
+            /***** Se houver alguma alteração de classes, deverá ser efetuado o Migration *****/
+            //config.MigrationCallback = (migration, oldSchemaVersion) =>
+            //{
+            //    if (oldSchemaVersion < SchemaVersion)
+            //    {
+            //        if (oldSchemaVersion < 1)
+            //        {
+            //            var newObj = migration.NewRealm.All<Class>();
+            //            // Use the dynamic api for oldPeople so we can access
+            //            // .FirstName and .LastName even though they no longer
+            //            // exist in the class definition.
+            //            var oldObj = migration.OldRealm.All("className");
+            //            for (var i = 0; i < newObj.Count(); i++)
+            //            {
+            //                var old = oldObj.ElementAt(i);
+            //                var new = newObj.ElementAt(i);
+            //            }
+            //        }
+            //    }
+            //};
+            #endregion
+
+            config.ObjectClasses = new[]
+            {
+                typeof(Agenda),
+                typeof(Apoio),
+                typeof(Informacao),
+                typeof(KeyValueStorage),
+                typeof(Nota),
+                typeof(Pessoa),
+                typeof(TimeLine)
+            };
+
+            return Realm.GetInstance(config);
+        }
+    }
+}
