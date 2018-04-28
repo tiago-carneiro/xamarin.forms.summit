c36011dd51494b7e7553271635950b09284b7dfd
Tiago Carneiro <web-tiago@hotmail.com>
2018-04-27 21:47:15 -0300

    chore(service): modelBase implementation at service

--- Core/Features/Main/InformacaoService.cs
+++ Core/Features/Main/InformacaoService.cs
@@ -11,9 +11,8 @@ namespace Xamarin.Summit
         Task LoadInformacoesAsync();
     }
 
-    public class InformacaoService : IInformacaoService
+    public class InformacaoService : ServiceBase, IInformacaoService
     {
-        ulong SchemaVersion => 0;
         IApiRest Api => RestService.For<IApiRest>(ConstantHelper.ApiUrl);
 
         public async Task LoadInformacoesAsync()
@@ -25,9 +24,9 @@ namespace Xamarin.Summit
             if (xamarinInfoResult == null)
                 return;
 
-            var pessoas = GetPessoas(xamarinInfoResult.Pessoas);
+            var pessoas = xamarinInfoResult.Pessoas.Select(s => s.ConvertTo<Pessoa>());
             var informacoes = GetInformacoes(xamarinInfoResult.Informacoes, pessoas);
-            var apoio = GetApoios(xamarinInfoResult.Apoio);
+            var apoio = xamarinInfoResult.Apoio.Select(s => s.ConvertTo<Apoio>());
             var agendas = GetAgendas(xamarinInfoResult.Agenda, pessoas);
 
             using (var tran = realm.BeginWrite())
@@ -38,7 +37,11 @@ namespace Xamarin.Summit
                 apoio.ToList().ForEach(a => realm.Add(a));
                 agendas.ToList().ForEach(a => realm.Add(a));
 
-                realm.Add(new KeyValueStorage { Key = KeyValueStorage.DataAtualizacao, Value = xamarinInfoResult.DataAtualizacao });
+                realm.Add(new KeyValueStorage
+                {
+                    Key = KeyValueStorage.DataAtualizacao,
+                    Value = xamarinInfoResult.DataAtualizacao
+                });
 
                 tran.Commit();
             }
@@ -46,37 +49,24 @@ namespace Xamarin.Summit
 
         Informacao GetInformacoes(InformacoesResult informacaoResult, IEnumerable<Pessoa> pessoas)
         {
-            var informacao = new Informacao
-            {
-                Descricao = informacaoResult.Descricao,
-                Lat = informacaoResult.Lat,
-                Local = informacaoResult.Local,
-                Lon = informacaoResult.Lon,
-                SubTitulo = informacaoResult.SubTitulo,
-                Titulo = informacaoResult.Titulo
-            };
+            var informacao = informacaoResult.ConvertTo<Informacao>();
+            informacaoResult.Notas.ToList().ForEach(n =>
+                informacao.Notas.Add(n.ConvertTo<Nota>()));
 
-            GetNotas(informacaoResult.Notas).ToList().ForEach(n => informacao.Notas.Add(n));
-            pessoas.Where(w => informacaoResult.Organizacao.Contains(w.Nome)).ToList().ForEach(p => informacao.Organizacao.Add(p));
+            pessoas.Where(w =>
+                informacaoResult.Organizacao.Contains(w.Nome)
+                ).ToList().ForEach(p =>
+                    informacao.Organizacao.Add(p));
 
             return informacao;
         }
-
-        IEnumerable<Nota> GetNotas(IEnumerable<NotaResult> notasResult)
-            => notasResult.Select(s => new Nota { Descricao = s.Descricao, Titulo = s.Titulo });
-
-        IEnumerable<Apoio> GetApoios(IEnumerable<ApoioResult> apoiosResult)
-            => apoiosResult.Select(s => new Apoio { Categoria = s.Categoria, Imagem = s.Imagem, Link = s.Link, Nome = s.Nome, Ordem = s.Ordem });
-
-        IEnumerable<Pessoa> GetPessoas(IEnumerable<PessoaResult> pessoasResult)
-            => pessoasResult.Select(s => new Pessoa { Descricao = s.Descricao, Imagem = s.Imagem, Link = s.Link, Nome = s.Nome, Titulo = s.Titulo });
 
         IEnumerable<Agenda> GetAgendas(IEnumerable<AgendaResult> agendasResult, IEnumerable<Pessoa> pessoas)
             => agendasResult.Select(s => GetAgenda(s, pessoas));
 
         Agenda GetAgenda(AgendaResult agendaResult, IEnumerable<Pessoa> pessoas)
         {
-            var agenda = new Agenda { Descricao = agendaResult.Descricao, Titulo = agendaResult.Titulo };
+            var agenda = agendaResult.ConvertTo<Agenda>();
             GetTimeLines(agendaResult.TimeLine, pessoas).ToList().ForEach(tl => agenda.TimeLine.Add(tl));
             return agenda;
         }
@@ -86,54 +76,16 @@ namespace Xamarin.Summit
 
         TimeLine GetTimeLine(TimeLineResult timeLineResult, IEnumerable<Pessoa> pessoas)
         {
-            var timeLine = new TimeLine { Descricao = timeLineResult.Descricao, Hora = timeLineResult.Hora, Titulo = timeLineResult.Titulo };
+            var timeLine = timeLineResult.ConvertTo<TimeLine>();
             if (!string.IsNullOrEmpty(timeLineResult.Palestrante))
             {
                 var nomes = timeLineResult.Palestrante.Split(',');
-                pessoas.Where(w => nomes.Contains(w.Nome)).ToList().ForEach(p => timeLine.Palestrantes.Add(p));
+                pessoas.Where(w =>
+                    nomes.Contains(w.Nome)
+                ).ToList().ForEach(p =>
+                    timeLine.Palestrantes.Add(p));
             }
             return timeLine;
-        }
-
-        Realm GetRealmInstance()
-        {
-            var config = new RealmConfiguration { SchemaVersion = SchemaVersion };
-
-            #region Migration
-            /***** Se houver alguma alteração de classes, deverá ser efetuado o Migration *****/
-            //config.MigrationCallback = (migration, oldSchemaVersion) =>
-            //{
-            //    if (oldSchemaVersion < SchemaVersion)
-            //    {
-            //        if (oldSchemaVersion < 1)
-            //        {
-            //            var newObj = migration.NewRealm.All<Class>();
-            //            // Use the dynamic api for oldPeople so we can access
-            //            // .FirstName and .LastName even though they no longer
-            //            // exist in the class definition.
-            //            var oldObj = migration.OldRealm.All("className");
-            //            for (var i = 0; i < newObj.Count(); i++)
-            //            {
-            //                var old = oldObj.ElementAt(i);
-            //                var new = newObj.ElementAt(i);
-            //            }
-            //        }
-            //    }
-            //};
-            #endregion
-
-            config.ObjectClasses = new[]
-            {
-                typeof(Agenda),
-                typeof(Apoio),
-                typeof(Informacao),
-                typeof(KeyValueStorage),
-                typeof(Nota),
-                typeof(Pessoa),
-                typeof(TimeLine)
-            };
-
-            return Realm.GetInstance(config);
         }
 
         void CleanDataBase(Realm realm)
