0c000f63184b80ed357e902b4a9bed549dcd12bc
Tiago Carneiro <web-tiago@hotmail.com>
2018-05-03 23:24:44 -0300

    feat(controls): Html formatted label

---
+++ Android/Controls/HtmlFormattedLabelRenderer.cs
@@ -0,0 +1,35 @@
+﻿using Android.Content;
+using Android.OS;
+using Android.Text;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Summit;
+using Xamarin.Summit.Android;
+
+[assembly: ExportRenderer(typeof(HtmlFormattedLabel), typeof(HtmlFormattedLabelRenderer))]
+namespace Xamarin.Summit.Android
+{
+    public class HtmlFormattedLabelRenderer : LabelRenderer
+    {
+        public HtmlFormattedLabelRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            var view = (HtmlFormattedLabel)Element;
+            if (view == null) return;
+            ISpanned htmlText;
+            var text = view.Text.ToString();
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.N)
+                htmlText = Html.FromHtml(text, Html.FromHtmlModeLegacy);
+            else
+                htmlText = Html.FromHtml(text);
+
+            Control.SetText(htmlText, TextView.BufferType.Spannable);
+        }
+    }
+}
---
+++ Core/Controls/HtmlFormattedLabel.cs
@@ -0,0 +1,8 @@
+﻿using Xamarin.Forms;
+
+namespace Xamarin.Summit
+{
+    public class HtmlFormattedLabel : Label
+    {
+    }
+}
