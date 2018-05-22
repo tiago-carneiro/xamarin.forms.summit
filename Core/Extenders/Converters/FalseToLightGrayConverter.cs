7f31e2b0f55907cc359fc9d4fe0e1753d40931ca
Tiago Carneiro <web-tiago@hotmail.com>
2018-05-22 20:50:47 -0300

    chore(layout): layout changes

---
+++ Core/Extenders/Converters/FalseToLightGrayConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Xamarin.Summit
+{
+    public class FalseToLightGrayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => (!(bool)value) ? "#ebebeb" : VisualElement.BackgroundColorProperty.DefaultValue;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
